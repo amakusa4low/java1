13192396175a92fb0664ba2f6061fd642595ce8c
Rentaro Otani <rencha1015@outlook.jp>
2021-10-19 01:26:29 +0900

    first add

---
+++ Greeting.java
@@ -0,0 +1,14 @@
+package first;
+
+/**
+ * Greeting
+ */
+class Greeting {
+  public static void main(String[] args){
+    System.out.println("Good morning");
+
+    System.out.println("Good afternoon");
+
+    System.out.println("Good evening");
+  }
+}
---
+++ Variable.java
@@ -0,0 +1,9 @@
+package first;
+
+class Variable {
+  public static void main(String[] args) {
+    // int var = 1;
+
+    // System.out.println(var);
+  }
+}
